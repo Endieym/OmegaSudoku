6ce07ae13018d4e2863d6e0ff16d8df2da5af413
Endieym <endieym2@gmail.com>
2022-12-28 18:50:43 +0200

    Added constants for UI

---
+++ OmegaSudoku/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku;
+
+internal static class Constants
+{
+
+    public const string Seperator = "#~----------------------------------~#";
+    public const string WelcomeMessage = "Welcome to Omega Sudoko Solver!" +
+        "\nThis program is designed to solve any difficulty of a Sudoko game" +
+        "\nEnter a sudoku position to be solved in the form of a string, like the following example:\n" +
+        "\n800000070006010053040600000000080400003000700020005038000000800004050061900002000";
+    public const string InputMessage = "Enter a string:";
+
+}
